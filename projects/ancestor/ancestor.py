27ff92bead0af9b060fa47520cb4ab2fab7cb521
cahaug <caleahaug@gmail.com>
2020-04-26 19:00:05 -0700

    working on ancestor still

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    oldest_ancestor = -1
+    relatives = {}
+    for parent_child_pair in ancestors:
+        if parent_child_pair[1] == starting_node:
+            if parent_child_pair[0] > oldest_ancestor:
+                oldest_ancestor = parent_child_pair[0]
+            relatives[parent_child_pair[0]] = parent_child_pair[1]
+    for parent_child_pair in ancestors:
+        if parent_child_pair[1] in relatives.keys():
+            if parent_child_pair[0] > oldest_ancestor:
+                oldest_ancestor = parent_child_pair[0]
+            relatives[parent_child_pair[0]] = parent_child_pair[1]
+    if len(relatives) > 0:
+        oldest_ancestor = list(relatives.keys())[-1]
+    print(relatives)
+    
+
+    return oldest_ancestor
